64b00ddcf5b9b1fc3fb0c59ec5c6d6d4454719f5
seqmwok <zzbz813@gmail.com>
2026-03-03 23:51:41 +0400

    nvim

--- init.lua
+++ init.lua
@@ -23,7 +23,7 @@ require("core.mappings")
 require("lazy").setup({
     spec = {
         { import = "plugins.editor" },
-        { import = "plugins.lsp" },
+        -- { import = "plugins.lsp" },
         { import = "plugins.tools" },
         { import = "plugins.ui" },
     }
